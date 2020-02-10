e51548e6b9224d92429e6e6b70f2e0aaf8bc26c5
Anis-Barghouthi <anis.barghouthi@outlook.com>
2020-02-10 23:59:41 +0100

    Add project files.

---
+++ Domain/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public DateTime DateProd { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public virtual ICollection<Provider> Providers { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
