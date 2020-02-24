1e312a91a57ec32abcdc71575ea4d59f909e9ed3
Anis-Barghouthi <anis.barghouthi@outlook.com>
2020-02-24 16:01:15 +0100

    DateTimeConvention

---
+++ Data/Migrations/202002241439378_Address.cs
@@ -0,0 +1,20 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Address : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "LabName", c => c.String());
+            DropColumn("dbo.Products", "City");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Products", "City", c => c.String());
+            DropColumn("dbo.Products", "LabName");
+        }
+    }
+}
